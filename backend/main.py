3b1da17a2e4f0d10f11098402aa02c398728e9da
Anadi Mehta <anadimehta9@gmail.com>
2026-06-30 16:26:25 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,425 @@
+
+from jose import jwt
+from passlib.context import CryptContext
+
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+import shutil
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from datetime import datetime
+
+app = FastAPI()
+load_dotenv()
+
+# ================= JWT =================
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+# ================= CORS =================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ================= MONGODB =================
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client["ujjain_samachar"]
+
+news_collection = db["news"]
+
+team_collection = db["team"]
+
+reels_collection = db["reels"]
+# ================= UPLOAD FOLDER =================
+
+UPLOAD_FOLDER = "uploads"
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# ================= STATIC FILES =================
+
+app.mount(
+    "/uploads",
+    StaticFiles(directory="uploads"),
+    name="uploads"
+)
+
+# ================= GET ALL NEWS =================
+
+@app.get("/news")
+def get_news():
+
+    news = list(
+        news_collection.find(
+            {},
+            {"_id": 0}
+        )
+    )
+
+    return news
+
+# ================= GET SINGLE NEWS =================
+
+@app.get("/news/{news_id}")
+def get_single_news(news_id: int):
+
+    news = news_collection.find_one(
+        {"id": news_id},
+        {"_id": 0}
+    )
+
+    if news:
+        return news
+
+    return {
+        "message": "News not found"
+    }
+
+# ================= UPLOAD NEWS =================
+
+@app.post("/upload-news")
+async def upload_news(
+    title: str = Form(...),
+    description: str = Form(...),
+    media: UploadFile = File(...)
+):
+
+    media_path = f"uploads/{media.filename}"
+
+    with open(media_path, "wb") as buffer:
+        shutil.copyfileobj(media.file, buffer)
+
+    news_count = news_collection.count_documents({})
+
+    new_news = {
+
+        "id": news_count + 1,
+
+        "title": title,
+
+        "description": description,
+
+        "media":
+            f"http://127.0.0.1:8000/{media_path}",
+
+        "type":
+            "video"
+            if media.content_type.startswith("video")
+            else "image",
+
+        "time":
+            datetime.now().strftime(
+                "%d %b %Y | %I:%M %p"
+            )
+            ,
+"likes": 0,
+"comments": []
+    }
+
+    news_collection.insert_one(new_news)
+
+    return {
+        "message": "News Uploaded Successfully"
+    }
+
+# ================= DELETE NEWS =================
+
+@app.delete("/delete-news/{news_id}")
+def delete_news(news_id: int):
+
+    news_collection.delete_one(
+        {"id": news_id}
+    )
+
+    return {
+        "message": "News Deleted Successfully"
+    }
+
+# ================= LOGIN =================
+
+@app.post("/login")
+def login(data: dict):
+
+    username = data.get("username")
+
+    password = data.get("password")
+
+    if (
+    username == os.getenv("ADMIN_USERNAME")
+    and
+    password == os.getenv("ADMIN_PASSWORD")
+):
+
+        token = jwt.encode(
+            {"username": username},
+            SECRET_KEY,
+            algorithm=ALGORITHM
+        )
+
+        return {
+            "token": token
+        }
+
+    return {
+        "message": "Invalid Credentials"
+    }
+
+# ================= UPDATE NEWS =================
+
+@app.put("/update-news/{news_id}")
+async def update_news(
+    news_id: int,
+    title: str = Form(...),
+    description: str = Form(...),
+    media: UploadFile = File(...)
+):
+
+    media_path = f"uploads/{media.filename}"
+
+    with open(media_path, "wb") as buffer:
+        shutil.copyfileobj(media.file, buffer)
+
+    news_collection.update_one(
+
+        {"id": news_id},
+
+        {
+            "$set": {
+
+                "title": title,
+
+                "description": description,
+
+                "media":
+                    f"http://127.0.0.1:8000/{media_path}",
+
+                "type":
+                    "video"
+                    if media.content_type.startswith("video")
+                    else "image"
+            }
+        }
+
+    )
+
+    return {
+        "message": "News Updated Successfully"
+    }
+# ================= LIKE NEWS =================
+
+@app.put("/like-news/{news_id}")
+def like_news(news_id: int):
+
+    news_collection.update_one(
+
+        {"id": news_id},
+
+        {
+            "$inc": {
+                "likes": 1
+            }
+        }
+
+    )
+
+    return {
+        "message": "Liked"
+    }
+# ================= ADD COMMENT =================
+
+@app.put("/comment-news/{news_id}")
+def comment_news(
+    news_id: int,
+    data: dict
+):
+
+    news_collection.update_one(
+
+        {"id": news_id},
+
+        {
+            "$push": {
+                "comments": data["comment"]
+            }
+        }
+
+    )
+
+    return {
+        "message": "Comment Added"
+    }
+# ================= BREAKING NEWS =================
+
+breaking_news = {
+    "text": "Welcome To Ujjain Samachar"
+}
+
+@app.get("/breaking-news")
+def get_breaking_news():
+
+    return breaking_news
+
+@app.put("/breaking-news")
+def update_breaking_news(data: dict):
+
+    breaking_news["text"] = data["text"]
+
+    return {
+        "message": "Breaking News Updated"
+    }
+# ================= ADD TEAM MEMBER =================
+
+@app.post("/add-team")
+async def add_team_member(
+    name: str = Form(...),
+    position: str = Form(...),
+    contact: str = Form(...),
+    dob: str = Form(...),
+    address: str = Form(...),
+    description: str = Form(...),
+    image: UploadFile = File(...)
+):
+
+    image_path = f"uploads/{image.filename}"
+
+    with open(image_path, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    team_count = team_collection.count_documents({})
+
+    new_member = {
+
+        "id": team_count + 1,
+
+        "name": name,
+
+        "position": position,
+
+        "contact": contact,
+
+        "dob": dob,
+
+        "address": address,
+
+        "description": description,
+
+        "image":
+            f"http://127.0.0.1:8000/{image_path}"
+    }
+
+    team_collection.insert_one(new_member)
+
+    return {
+        "message": "Team Member Added"
+    }
+
+
+# ================= GET TEAM MEMBERS =================
+
+@app.get("/team")
+def get_team_members():
+
+    members = list(
+        team_collection.find(
+            {},
+            {"_id": 0}
+        )
+    )
+
+    return members
+
+
+# ================= DELETE TEAM MEMBER =================
+
+@app.delete("/delete-team/{member_id}")
+def delete_team_member(member_id: int):
+
+    team_collection.delete_one(
+        {"id": member_id}
+    )
+
+    return {
+        "message": "Member Deleted"
+    }
+# ================= UPLOAD REEL =================
+
+@app.post("/upload-reel")
+async def upload_reel(
+
+    title: str = Form(...),
+
+    video: UploadFile = File(...)
+
+):
+
+    video_path = f"uploads/{video.filename}"
+
+    with open(video_path, "wb") as buffer:
+        shutil.copyfileobj(video.file, buffer)
+
+    reel_count =reels_collection.count_documents({})
+
+    new_reel = {
+
+        "id": reel_count + 1,
+
+        "title": title,
+
+        "video":
+            f"http://127.0.0.1:8000/{video_path}"
+    }
+
+    reels_collection.insert_one(new_reel)
+
+    return {
+        "message": "Reel Uploaded"
+    }
+
+
+# ================= GET REELS =================
+
+@app.get("/reels")
+def get_reels():
+
+    reels = list(
+
+        reels_collection.find(
+            {},
+            {"_id": 0}
+        )
+
+    )
+
+    return reels
+
+
+# ================= DELETE REEL =================
+
+@app.delete("/delete-reel/{reel_id}")
+def delete_reel(reel_id: int):
+
+    reels_collection.delete_one(
+        {"id": reel_id}
+    )
+
+    return {
+        "message": "Reel Deleted"
+    }
---
+++ frontend/script.js
@@ -0,0 +1,530 @@
+async function loadNews() {
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/news"
+  );
+
+  const newsData = await response.json();
+
+  const container = document.getElementById(
+    "newsContainer"
+  );
+
+  container.innerHTML = "";
+
+  newsData.forEach(news => {
+
+    const card = document.createElement("div");
+
+    card.classList.add("news-card");
+
+    card.innerHTML = `
+
+      ${
+        news.type === "video"
+
+        ?
+
+        `
+          <video controls width="100%">
+            <source src="${news.media}">
+          </video>
+        `
+
+        :
+
+        `
+          <img src="${news.media}" alt="news">
+        `
+      }
+
+      <div class="news-content">
+
+        <h3>${news.title}</h3>
+
+        <p>${news.description}</p>
+
+        <small style="
+          color:gray;
+          display:block;
+          margin-top:10px;
+        ">
+          ${news.time || "Just Now"}
+        </small>
+
+        <div style="margin-top:15px;">
+
+          ${
+            news.comments
+            ?
+
+            news.comments.map(comment => `
+
+              <p style="
+                background:#f1f1f1;
+                padding:8px;
+                border-radius:8px;
+                margin-top:5px;
+              ">
+                💬 ${comment}
+              </p>
+
+            `).join("")
+
+            :
+
+            ""
+          }
+
+        </div>
+
+        <div style="margin-top:15px;">
+
+          <input
+            type="text"
+            id="comment-${news.id}"
+            placeholder="Write comment..."
+            onkeypress="
+  if(event.key === 'Enter'){
+    addComment(${news.id})
+  }
+"
+            style="
+              width:100%;
+              padding:10px;
+              border-radius:8px;
+              border:1px solid #ccc;
+              margin-bottom:10px;
+            "
+          >
+
+          <button
+            onclick="addComment(${news.id})"
+            class="news-btn"
+          >
+            Comment
+          </button>
+
+        </div>
+
+        <button
+          onclick="likeNews(${news.id})"
+          class="news-btn"
+        >
+          ❤️ ${news.likes || 0}
+        </button>
+
+        <a href="news.html?id=${news.id}">
+
+          <button class="news-btn">
+            Read More
+          </button>
+
+        </a>
+
+      </div>
+
+    `;
+
+    container.appendChild(card);
+
+  });
+
+}
+
+loadNews();
+
+document
+  .getElementById("searchInput")
+  .addEventListener("input", searchNews);
+
+async function searchNews(){
+
+  const searchValue =
+    document
+      .getElementById("searchInput")
+      .value
+      .toLowerCase();
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/news"
+  );
+
+  const newsData = await response.json();
+
+  const filteredNews =
+    newsData.filter(news =>
+
+      news.title
+        .toLowerCase()
+        .includes(searchValue)
+
+    );
+
+  const container =
+    document.getElementById("newsContainer");
+
+  container.innerHTML = "";
+
+  filteredNews.forEach(news => {
+
+    const card = document.createElement("div");
+
+    card.classList.add("news-card");
+
+    card.innerHTML = `
+
+      ${
+        news.type === "video"
+
+        ?
+
+        `
+          <video controls width="100%">
+            <source src="${news.media}">
+          </video>
+        `
+
+        :
+
+        `
+          <img src="${news.media}" alt="news">
+        `
+      }
+
+      <div class="news-content">
+
+        <h3>${news.title}</h3>
+
+        <p>${news.description}</p>
+
+        <small style="
+          color:gray;
+          display:block;
+          margin-top:10px;
+        ">
+          ${news.time || "Just Now"}
+        </small>
+
+        <div style="margin-top:15px;">
+
+          ${
+            news.comments
+            ?
+
+            news.comments.map(comment => `
+
+              <p style="
+                background:#f1f1f1;
+                padding:8px;
+                border-radius:8px;
+                margin-top:5px;
+              ">
+                💬 ${comment}
+              </p>
+
+            `).join("")
+
+            :
+
+            ""
+          }
+
+        </div>
+
+        <div style="margin-top:15px;">
+
+          <input
+            type="text"
+            id="comment-${news.id}"
+            placeholder="Write comment..."
+            onkeypress="
+  if(event.key === 'Enter'){
+    addComment(${news.id})
+  }
+"
+            style="
+              width:100%;
+              padding:10px;
+              border-radius:8px;
+              border:1px solid #ccc;
+              margin-bottom:10px;
+            "
+          >
+
+          <button
+            onclick="addComment(${news.id})"
+            class="news-btn"
+          >
+            Comment
+          </button>
+
+        </div>
+
+        <button
+          onclick="likeNews(${news.id})"
+          class="news-btn"
+        >
+          ❤️ ${news.likes || 0}
+        </button>
+
+        <a href="news.html?id=${news.id}">
+
+          <button class="news-btn">
+            Read More
+          </button>
+
+        </a>
+
+      </div>
+
+    `;
+
+    container.appendChild(card);
+
+  });
+
+}
+
+async function likeNews(id){
+
+  await fetch(
+    `http://127.0.0.1:8000/like-news/${id}`,
+    {
+      method:"PUT"
+    }
+  );
+
+  loadNews();
+
+}
+
+async function addComment(id){
+
+  const commentInput =
+    document.getElementById(
+      `comment-${id}`
+    );
+
+  const comment =
+    commentInput.value;
+
+  if(!comment) return;
+
+  await fetch(
+
+    `http://127.0.0.1:8000/comment-news/${id}`,
+
+    {
+      method:"PUT",
+
+      headers:{
+        "Content-Type":"application/json"
+      },
+
+      body: JSON.stringify({
+        comment
+      })
+    }
+
+  );
+
+  loadNews();
+
+}
+async function addComment(id){
+
+  const commentInput =
+    document.getElementById(
+      `comment-${id}`
+    );
+
+  const comment =
+    commentInput.value;
+
+  if(!comment) return;
+
+  await fetch(
+
+    `http://127.0.0.1:8000/comment-news/${id}`,
+
+    {
+      method:"PUT",
+
+      headers:{
+        "Content-Type":"application/json"
+      },
+
+      body: JSON.stringify({
+        comment
+      })
+    }
+
+  );
+
+  loadNews();
+
+}
+async function loadBreakingNews(){
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/breaking-news"
+  );
+
+  const data = await response.json();
+
+  document.getElementById(
+    "breakingText"
+  ).innerText = data.text;
+
+}
+
+loadBreakingNews();
+async function loadTeam(){
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/team"
+  );
+
+  const teamData = await response.json();
+
+  const container =
+    document.getElementById(
+      "teamContainer"
+    );
+
+  if(!container) return;
+
+  container.innerHTML = "";
+
+  teamData.forEach(member => {
+
+    container.innerHTML += `
+
+      <div class="team-card">
+
+        <div class="team-image-box">
+
+          <img
+            src="${member.image}"
+          >
+
+        </div>
+
+        <h3>${member.name}</h3>
+
+        <span class="team-role">
+          ${member.position}
+        </span>
+
+        <p class="team-description">
+          ${member.description}
+        </p>
+
+        <div class="team-info">
+
+          <p>
+            📞 ${member.contact}
+          </p>
+
+          <p>
+            🎂 ${member.dob}
+          </p>
+
+          <p>
+            📍 ${member.address}
+          </p>
+
+        </div>
+
+      </div>
+
+    `;
+
+  });
+
+}
+
+loadTeam();
+
+/* ========= PREMIUM HOME REELS ========= */
+
+async function loadHomeReels(){
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/reels"
+  );
+
+  const reels = await response.json();
+
+  const container =
+    document.getElementById(
+      "homeReels"
+    );
+
+  if(!container) return;
+
+  container.innerHTML = "";
+
+  reels.slice(0,4).forEach(reel => {
+
+    const card =
+      document.createElement("div");
+
+    card.classList.add("reel-card");
+
+    card.innerHTML = `
+
+      <video muted>
+
+        <source src="${reel.video}">
+
+      </video>
+
+      <div class="reel-overlay"></div>
+
+      <div class="reel-play">
+
+        ▶
+
+      </div>
+
+      <div class="reel-info">
+
+        <span class="reel-tag">
+          TRENDING
+        </span>
+
+        <h3>
+          ${reel.caption || "Ujjain Update"}
+        </h3>
+
+        <p>
+          Ujjain Samachar • Live Coverage
+        </p>
+
+      </div>
+
+    `;
+
+    card.onclick = () => {
+
+      window.location.href =
+        "reels.html";
+
+    };
+
+    container.appendChild(card);
+
+  });
+
+}
+
+loadHomeReels();
+
+function openReels(){
+
+  window.location.href =
+    "reels.html";
+
+}
